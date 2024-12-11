956124a5f965391584b2d3720d96f7060bcd532c
Abbas285 <73587440+Abbas285@users.noreply.github.com>
2024-12-11 20:23:30 +0500

    update

--- src/app/components/GenerationComp.tsx
+++ src/app/components/GenerationComp.tsx
@@ -4,11 +4,13 @@ import file from "@/assets/file.png"
 import layoutCloseIccon from "@/assets/layoutCloseIccon.png"
 import generationIcon from "@/assets/generation.svg"
 import wireframes from "@/assets/wireframes.png"
+import { CiMobile2 } from "react-icons/ci";
+import { MdLaptopChromebook } from "react-icons/md";
 const Generation = () => {
     return (
         <div className="w-full flex justify-start items-center">
             <div className="  px-3 py-2 bg-white rounded-md border
-             shadow-md cursor-pointer hover:scale-105 transition-all">
+             shadow-md cursor-pointer hover:scale-105 transition-all w-[85%]">
                 <div className='flex  items-center justify-between'>
                     <div className='flex gap-2 items-center'>
                         <Image
@@ -37,8 +39,8 @@ const Generation = () => {
                     <h3 className="text-base font-semibold text-[#48484D]">
                         Design Mode
                     </h3>
-                    <div className='flex gap-5 pt-2'>
-                        <div className='flex-col flex  items-center border hover:border-2 px-4 py-2 rounded-md hover:border-[#3F20FB]'>
+                    <div className='flex justify-between pt-2'>
+                        <div className='flex-col flex  items-center border hover:border-2 px-3 py-2 rounded-md hover:border-[#3F20FB]'>
                             <Image
                                 src={wireframes}
                                 alt="wireframes"
@@ -52,7 +54,8 @@ const Generation = () => {
                                 Wireframes
                             </h3>
                         </div>
-                        <div className='flex-col flex  items-center border hover:border-2 px-4 py-2 rounded-md hover:border-[#3F20FB]'>
+                        <div className='flex-col flex  items-center border hover:border-2
+                         px-3 py-2 rounded-md hover:border-[#3F20FB]'>
                             <Image
                                 src={file}
                                 alt="file"
@@ -65,6 +68,63 @@ const Generation = () => {
                             <h3 className="text-base hover:font-medium hover:text-[#3F20FB] font-normal text-[#606067]">
                                 Hifi Designs
                             </h3>
+                        </div>
+                    </div>
+                    <div className='w-full flex justify-between pt-5 items-center'>
+
+                        <h3 className="text-base font-semibold text-[#48484D]">
+                            Screen type
+                        </h3>
+                        <div className='flex  border rounded-md  items-center'>
+                            <div className='p-2 hover:bg-[#ece9ff]  hover:rounded-l-sm hover:text-[#3F20FB]'>
+                                <MdLaptopChromebook size={20} className='' />
+                            </div>
+                            <div className='p-2 hover:bg-[#ece9ff]  hover:rounded-r-sm'>
+                                <CiMobile2 size={20} className='' />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='w-full flex justify-between pt-5 items-center'>
+
+                        <h3 className="text-base font-semibold text-[#48484D]">
+                            From
+                        </h3>
+                        <div className='flex  border rounded-md  items-center text-[#606067]'>
+                            <div className='p-2 hover:bg-[#ece9ff]  hover:rounded-l-sm hover:text-[#3F20FB] '>
+                                Prompt
+                            </div>
+                            <div className='p-2 hover:bg-[#ece9ff]  hover:rounded-r-sm hover:text-[#3F20FB]'>
+                                Diagram
+                            </div>
+                        </div>
+                    </div>
+                    <div className='w-full flex flex-col pt-5 '>
+                        <h3 className="text-base font-semibold text-[#303034] pb-1">
+                            Prompt
+                        </h3>
+                        <p className='pt-2 font-medium text-sm leading-5 text-[#606067] p-1.5  border hover:border-2
+                         rounded-md hover:border-[#3F20FB]'>
+                            landing page for a travel booking website,
+                            highlighting vacation packages, user testimonials,
+                            and a search bar|</p>
+                    </div>
+                    <div className='w-full flex flex-col pt-5 '>
+                        <h3 className="text-base font-semibold text-[#303034] pb-1">
+                            Number of variations
+                        </h3>
+                        <div className='flex  border rounded-md  text-[#303034]'>
+                            <div className='p-2 hover:bg-[#ece9ff]  hover:rounded-l-sm  w-1/4 text-center hover:text-[#3F20FB]'>
+                                1
+                            </div>
+                            <div className='p-2 hover:bg-[#ece9ff]   w-1/4 text-center hover:text-[#3F20FB]'>
+                                2
+                            </div>
+                            <div className='p-2 hover:bg-[#ece9ff]   w-1/4 text-center hover:text-[#3F20FB]'>
+                                3
+                            </div>
+                            <div className='p-2 hover:bg-[#ece9ff]  hover:rounded-r-sm w-1/4 text-center hover:text-[#3F20FB]'>
+                                4
+                            </div>
                         </div>
                     </div>
                 </div>
