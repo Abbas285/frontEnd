e54e2e2f0e165dad1268b767cda9215a1de182fd
Abbas285 <73587440+Abbas285@users.noreply.github.com>
2024-12-11 18:05:11 +0500

    update

--- src/app/components/DiagramComp.tsx
+++ src/app/components/DiagramComp.tsx
@@ -41,44 +41,14 @@ const Diagram = () => {
                         className="w-[32px] h-[32px] object-contain"
                     />
                     <MdLaptopChromebook size={22} color='#606067' />
-                    <SlOptionsVertical size={20} color='#606067' className='ml-8 cursor-pointer hover:scale-105 transition-all' />
+                    <SlOptionsVertical size={20} color='#606067' className='ml-6 
+                    cursor-pointer hover:scale-105 transition-all' />
                 </div>
             </div>
 
-            {/* <div className="w-full max-w-4xl flex justify-between items-center bg-white shadow-sm rounded-md px-4 py-2 mb-6">
-                <h2 className="text-lg font-medium text-gray-800">Generation 1</h2>
-                <div className="relative">
-
-                    <button className="flex items-center text-gray-600 hover:text-gray-800">
-                        <span className="mr-2">More Options</span>
-
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            className="w-5 h-5"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M12 6.75a.75.75 0 100-1.5.75.75 0 000 1.5zm0 5.25a.75.75 0 100-1.5.75.75 0 000 1.5zm0 5.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-                            />
-                        </svg>
-                    </button>
-
-                    <div className="absolute right-0 mt-2 w-40 bg-white rounded-md shadow-lg py-1 hidden group-hover:block">
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Generate Images</a>
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Generate Screens Flow</a>
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Source Code</a>
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Export as...</a>
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Share</a>
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Delete forever</a>
-                    </div>
-                </div>
+            {/* <div className='w-full bg-[#dcdcde] border-dotted rounded border-[##524d4d] border-[2px] px-7'>
+               
             </div> */}
-
             <div className="w-full max-w-4xl bg-white rounded-lg shadow-sm">
                 <div className="h-[calc(100vh-200px)] bg-gray-200 rounded-lg"></div>
             </div>
