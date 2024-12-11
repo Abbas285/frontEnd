2a8002234b8bae1cd9dc765cf4be14f5fb1b8ed2
Abbas285 <73587440+Abbas285@users.noreply.github.com>
2024-12-11 16:03:39 +0500

    update

--- src/app/components/Generation.tsx
+++ src/app/components/Generation.tsx
@@ -1,22 +1,34 @@
 import React from 'react'
-import generationIcon from "@/assets/generation.svg"
 import Image from 'next/image'
+import flip from "@/assets/flip.png"
+import generationIcon from "@/assets/generation.svg"
+
 
 const Generation = () => {
     return (
         <div className="w-full flex justify-start items-center">
-            <div className="flex items-center gap-2 px-4 py-2 bg-white rounded-full border shadow-sm cursor-pointer">
-                <Image
-                    src={generationIcon}
-                    alt="generationIcon"
-                    width={32}
-                    height={32}
+            <div className="flex items-center  px-4 py-2 bg-white rounded-full border shadow-sm cursor-pointer">
+                <div className='flex gap-2 items-center justify-between'>
+                    <Image
+                        src={generationIcon}
+                        alt="generationIcon"
+                        width={32}
+                        height={32}
+                        quality={100}
+                        className="w-[32px] h-[32px] object-contain"
+                    />
+                    <h3 className="text-base font-medium text-[#48484D]">
+                        Generation
+                    </h3>
+                </div>
+                {/* <Image
+                    src={flip}
+                    alt="flip"
+                    width={16}
+                    height={16}
                     quality={100}
-                    className="w-[32px] h-[32px] object-contain"
-                />
-                <h3 className="text-base font-medium text-[#48484D]">
-                    Generation
-                </h3>
+                    className="w-[16px] h-[16px] object-contain cursor-pointer"
+                /> */}
             </div>
         </div>
 
