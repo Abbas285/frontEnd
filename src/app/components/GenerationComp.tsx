eb7ad0d50c31f84449712e2beb47e51420101da6
Abbas285 <73587440+Abbas285@users.noreply.github.com>
2024-12-11 20:53:40 +0500

    update

--- src/app/components/GenerationComp.tsx
+++ src/app/components/GenerationComp.tsx
@@ -6,11 +6,12 @@ import generationIcon from "@/assets/generation.svg"
 import wireframes from "@/assets/wireframes.png"
 import { CiMobile2 } from "react-icons/ci";
 import { MdLaptopChromebook } from "react-icons/md";
+import generateWhiteIcon from "@/assets/generateWhiteIcon.png"
 const Generation = () => {
     return (
         <div className="w-full flex justify-start items-center">
             <div className="  px-3 py-2 bg-white rounded-md border
-             shadow-md cursor-pointer hover:scale-105 transition-all w-[85%]">
+             shadow-md cursor-pointer  w-[85%]">
                 <div className='flex  items-center justify-between'>
                     <div className='flex gap-2 items-center'>
                         <Image
@@ -112,20 +113,37 @@ const Generation = () => {
                         <h3 className="text-base font-semibold text-[#303034] pb-1">
                             Number of variations
                         </h3>
-                        <div className='flex  border rounded-md  text-[#303034]'>
-                            <div className='p-2 hover:bg-[#ece9ff]  hover:rounded-l-sm  w-1/4 text-center hover:text-[#3F20FB]'>
-                                1
-                            </div>
-                            <div className='p-2 hover:bg-[#ece9ff]   w-1/4 text-center hover:text-[#3F20FB]'>
-                                2
-                            </div>
-                            <div className='p-2 hover:bg-[#ece9ff]   w-1/4 text-center hover:text-[#3F20FB]'>
-                                3
-                            </div>
-                            <div className='p-2 hover:bg-[#ece9ff]  hover:rounded-r-sm w-1/4 text-center hover:text-[#3F20FB]'>
-                                4
-                            </div>
+                        <div className='flex  border rounded-md  text-[#303034]'>{
+
+                            [1, 2, 3, 4].map((item, index) => {
+                                return <div key={index} className='p-2 hover:bg-[#ece9ff]  hover:rounded-l-sm  w-1/4 text-center hover:text-[#3F20FB]'>
+                                    {item}
+                                </div>
+
+                            })
+                        }
+
+
                         </div>
+                        <button
+                            className="mt-6 py-2 flex justify-center items-center text-[#f0eff4]
+             font-semibold text-base w-full rounded-full cursor-pointer 
+             hover:scale-105 transition-all gap-2"
+                            style={{
+                                background: "radial-gradient(96.83% 118.33% at 47.79% 5%, #3F20FB 0%, #B377FF 100%)",
+                            }}
+                        >
+                            <Image
+                                src={generateWhiteIcon}
+                                alt="generateWhiteIcon"
+                                width={22}
+                                height={22}
+                                quality={100}
+                                className="w-[22px] h-[22px] object-contain"
+                            />
+                            Generate
+                        </button>
+
                     </div>
                 </div>
 
